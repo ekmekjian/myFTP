a56ab191dd49e7e23a54e9585413ab1b6827dd65
Spawndemonic <spawndemonic@gmail.com>
2020-02-23 18:34:40 -0500

    initial commit

---
+++ client.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<string.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include "tools.h"
+#define PORT 7890
+char * ITOA(int num){
+  char * crvt=malloc(sizeof(char*)*5);
+  snprintf(crvt,5,"%d",num);
+return crvt;
+}
+char * message(char*key,int num)
+{
+  char * msg=malloc(sizeof(char)*50);
+  strcpy(msg,key);
+  strcat(msg,ITOA(num));
+  return msg;
+}
+
+int main(void){
+  int socketfd;
+  struct sockaddr_in server_addr;
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(PORT);
+  server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  socklen_t addr_size;
+  char buffer[1024];
+  char * key = malloc(sizeof(char*)*34); 
+  key = "UoMYTrfrBFHyQXmg6gzctqAwOmw1IohZ ";
+
+  if((socketfd = socket(AF_INET,SOCK_STREAM,0))==-1)
+    fatal("In Socket");
+  if((connect(socketfd, (struct sockaddr *) &server_addr,sizeof(server_addr)))==-1)
+    fatal("Connection");
+     //recv(int socket, void *buffer, size_t length, int flags) 
+     //recv(socketfd, buffer, 1024,0);
+     //printf("%s \n",buffer);
+  while(1){
+    for(int i=1000;i<10000;i++){
+    char * msg;
+    msg = message(key,i);
+    //send(int socket, const void *buffer, size_t length, int flags)
+    if(send(socketfd,msg,strlen(msg)+1,0)<1)
+      fatal("Sending");
+
+    }
+  }
+  close(socketfd);
+}
---
+++ server.c
@@ -0,0 +1,51 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "tools.h"
+
+#define PORT 7890
+
+int main(void){
+  struct sockaddr_in host_addr, client_addr;
+  socklen_t sin_size;
+  int recv_length=1, yes=1;
+  char buffer[1024];
+  int socketfd, clientfd;
+
+  host_addr.sin_family = AF_INET;//assigning IPV4 address family
+  host_addr.sin_port = htons(PORT);//convert port number to netowork short
+  host_addr.sin_addr.s_addr = htonl(INADDR_ANY);//automatically assign IP
+  memset(&(host_addr.sin_zero), '\0', 8);
+
+  if((socketfd = socket(AF_INET,SOCK_STREAM,0))==-1)
+    fatal("In Socket");
+
+  if(bind(socketfd, (struct sockaddr *)&host_addr, sizeof(struct sockaddr))==-1)
+     fatal("binding to socket");
+  if (listen(socketfd, 5) == -1)
+    fatal("listening on socket");
+
+  while(1){
+    sin_size = sizeof(struct sockaddr_in);
+    clientfd = accept(socketfd, (struct sockaddr *)&client_addr, &sin_size);
+    if(clientfd == -1)
+      fatal("accepting connection");
+    printf("server: got connection from %s port %d\n",
+        inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+    send(clientfd, "Hello, world!\n",13,0);
+    while(recv_length > 0){
+      printf("RECV: %d bytes\n", recv_length);
+      dump(buffer, recv_length);
+     //recv(socketfd, buffer, 1024,0);
+      recv_length = recv(clientfd, &buffer, 1024, 0);
+    }
+    shutdown(clientfd,2);
+    close(clientfd);
+
+  }
+  return 0;
+}
